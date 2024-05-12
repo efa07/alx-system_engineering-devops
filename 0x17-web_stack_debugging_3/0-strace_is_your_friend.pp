72c54e20428cd5666042541be46b3fb3d8fb2135
efa07 <efatariku07@gmail.com>
2024-05-12 09:56:59 +0300

    puupet code

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Puppet code to fix the bug in wp-setings.php
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
